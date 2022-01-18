e646301610996e028bbd5262b913f9317ad699f4
Stephan Merker <stephan.merker@sap.com>
2022-01-18 18:39:30 +0100

    Update project
    - Java 17
    - java buildpack 4.47
    - maven plugin managment: use recent versions

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -1,22 +1,21 @@
 package org.example;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-      throws ServletException, IOException
-    {
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
         // Very simple - just return some plain text
-    	response.setContentType("text/plain;charset=utf-8");
+        response.setContentType("text/plain;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("Java is an island");
+        response.getWriter().println("Hello Java!");
     }
 }
